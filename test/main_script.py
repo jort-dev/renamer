34af53320c026b2276c65c976839b964569cb623
Jort <jort.developer@gmail.com>
2023-03-25 01:02:23 +0100

    remove test dir

--- test/main_script.py
+++
@@ -1,9 +0,0 @@
-# i want to compile the program but still let the user use a fully fledged python script
-# cant just load it each time, needs to stay in memory for variables to work
-filename = "dynamic_script.py"
-with open(filename, 'r') as file:
-    code = file.read()
-
-print(code)
-result = eval(code)
-print(result)
